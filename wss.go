61e1a9dc7a90a0a3499b783e2340f1ca631b8e6e
root <lj@vipex.cc>
2022-07-01 21:03:54 +0800

    .

--- wss.go
+++ wss.go
@@ -10,26 +10,18 @@ import (
 	"fmt"
 )
 
-var wss = websocket.Upgrader{}
+var wss = websocket.Upgrader{} //
+
+func run(shCmd string) *bufio.Reader {
+    cmd := exec.Command("sh", /*"-x",*/ "-c", shCmd+";echo -n \\#", "2>&1"); stdout,_ := cmd.StdoutPipe(); bufReader := bufio.NewReader(stdout); cmd.Stderr = os.Stderr; go cmd.Run(); return bufReader //
+}
 
 func main() {
 	r := gin.Default()
   r.LoadHTMLGlob("./*.htm")
 	r.GET("/", func(c *gin.Context) {
       if c.Query("id") != "" {
-        // wss := websocket.Upgrader{}
-        con, _ := wss.Upgrade(c.Writer, c.Request, nil)
-        defer con.Close()
-        cmd := exec.Command("sh", /*"-x",*/ "-c", "packer build -var version_kic=b725891a packer-kic.json;echo -n \\#", "2>&1")
-        stdout,_ := cmd.StdoutPipe()
-        bufReader := bufio.NewReader(stdout)
-        cmd.Stderr = os.Stderr
-        cmd.Start()
-        for {
-          log,_,_ := bufReader.ReadLine(); if string(log) == "#" { return }; con.WriteMessage(1, log)
-        }
-        cmd.Wait()
-        fmt.Println(".")
+        con, _ := wss.Upgrade(c.Writer, c.Request, nil); defer con.Close(); bufReader := run("packer --help"); for { log,_,_ := bufReader.ReadLine(); if string(log) == "#" { break }; con.WriteMessage(1, []byte("{\""+c.Query("id")+"\":\""+string(log)+"\"}")) }; fmt.Println("shCmd exec finished...")
       } else {
         c.HTML(200, "default.htm", nil) //
       }
