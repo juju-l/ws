b9573796e389062b303692a5f383fb3fc8048272
root <lj@vipex.cc>
2022-07-01 10:54:54 +0800

    .

--- wss.go
+++ wss.go
@@ -10,16 +10,16 @@ import (
 
 var upGrader = websocket.Upgrader{}
 
-func wsHandle(rsp http.ResponseWriter, req *http.Request) {
-  ws, _ := upGrader.Upgrade(rsp, req, nil)
+func wssHandle(rsp http.ResponseWriter, req *http.Request) {
+  wss, _ := upGrader.Upgrade(rsp, req, nil)
   cmd := exec.Command("packer", "build", "-var version_kic=b725891a", "packer-kic.json", "2>&1")
   stdout,_ := cmd.StdoutPipe(); soBuffer := bufio.NewReader(stdout); //cmd.Stderr = os.Stderr
-  cmd.Start(); for /*range time.Tick(0.1 * time.Second)*/ { line,_,_ := soBuffer.ReadLine(); ws.WriteMessage(websocket.TextMessage, line); fmt.Println(string( line )) }; //cmd.Wait()
+  cmd.Start(); for /*range time.Tick(0.1 * time.Second)*/ { line,_,_ := soBuffer.ReadLine(); wss.WriteMessage(websocket.TextMessage, line); fmt.Println(string( line )) }; //cmd.Wait()
   fmt.Println("test")
 }
 
 func main() {
-  http.HandleFunc("/fw", wsHandle)
+  http.HandleFunc("/fw", wssHandle)
   http.HandleFunc("/", func(rsp http.ResponseWriter, req *http.Request) {
     http.ServeFile(rsp, req, "default.htm")
   })
