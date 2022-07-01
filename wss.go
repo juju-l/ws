2187dbb86e82d6e98c47eb318f8c7083265b65a8
root <lj@vipex.cc>
2022-07-01 10:58:18 +0800

    .

--- wss.go
+++ wss.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"net/http"
 	"github.com/gorilla/websocket"
+	"os"
 	"os/exec"
+	//   "time"
 	"fmt"
 )
 
@@ -13,7 +15,7 @@ var upGrader = websocket.Upgrader{}
 func wssHandle(rsp http.ResponseWriter, req *http.Request) {
   wss, _ := upGrader.Upgrade(rsp, req, nil)
   cmd := exec.Command("packer", "build", "-var version_kic=b725891a", "packer-kic.json", "2>&1")
-  stdout,_ := cmd.StdoutPipe(); soBuffer := bufio.NewReader(stdout); //cmd.Stderr = os.Stderr
+  stdout,_ := cmd.StdoutPipe(); soBuffer := bufio.NewReader(stdout); cmd.Stderr = os.Stderr
   cmd.Start(); for /*range time.Tick(0.1 * time.Second)*/ { line,_,_ := soBuffer.ReadLine(); wss.WriteMessage(websocket.TextMessage, line); fmt.Println(string( line )) }; //cmd.Wait()
   fmt.Println("test")
 }
