87ab458c231d8f0301bedbdb3d179b112e615b2a
root <lj@vipex.cc>
2022-07-01 13:12:51 +0800

    .

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module websocket
 
 require (
-	//github.com/gin-gonic/gin v1.8.0
+	github.com/gin-gonic/gin v1.8.0
 	github.com/gorilla/websocket v1.5.0
 	//golang.org/x/text v0.3.7
 )
--- wss.go
+++ wss.go
@@ -1,31 +1,43 @@
 package main
 
 import (
-	"bufio"
-	"net/http"
-	"github.com/gorilla/websocket"
 	"os"
 	"os/exec"
-	//   "time"
+    // "time"
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	"bufio"
 	"fmt"
 )
 
-var upGrader = websocket.Upgrader{}
-
-func wssHandle(rsp http.ResponseWriter, req *http.Request) {
-  wss, _ := upGrader.Upgrade(rsp, req, nil)
-  cmd := exec.Command("sh", /*"-x",*/ "-c", "packer build -var version_kic=b725891a packer-kic.json;echo cmdFinished", "2>&1")
-  stdout,_ := cmd.StdoutPipe(); soBuffer := bufio.NewReader(stdout); cmd.Stderr = os.Stderr
-  cmd.Start(); for /*range time.Tick(0.1 * time.Second)*/ { log,_,_ := soBuffer.ReadLine(); if string(log) == "cmdFinished" { return }; wss.WriteMessage(websocket.TextMessage, log) }; //cmd.Wait()
-  fmt.Println("test")
-}
-
 func main() {
-  http.HandleFunc("/fw", wssHandle)
-  http.HandleFunc("/", func(rsp http.ResponseWriter, req *http.Request) {
-    http.ServeFile(rsp, req, "default.htm")
-  })
-  http.ListenAndServe(":8080", nil)
+	r := gin.Default()
+  r.LoadHTMLGlob("./*.htm")
+	r.GET("/", func(c *gin.Context) {
+      if c.Query("id") != "" {
+        wss := websocket.Upgrader{}
+        con, _ := wss.Upgrade(c.Writer, c.Request, nil)
+        defer con.Close()
+        cmd := exec.Command("sh", /*"-x",*/ "-c", "packer build -var version_kic=b725891a packer-kic.json;echo \\#", "2>&1")
+        stdout,_ := cmd.StdoutPipe()
+        soBuffer := bufio.NewReader(stdout)
+        cmd.Stderr = os.Stderr
+        cmd.Start()
+        for {
+          log,_,_ := soBuffer.ReadLine()
+          if string(log) == "#" {
+            return
+          }
+          con.WriteMessage(1, log)
+        }
+        // cmd.Wait()
+        // fmt.Println(".")
+      } else {
+        c.HTML(200, "default.htm", nil)
+      }
+    })
+  ///
+	r.Run(":8080")
 }
 
 func init() {
