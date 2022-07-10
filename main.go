7fde4da577ac8f879a67d16fc9e1c654e2b03434
root <lj@vipex.cc>
2022-07-10 11:51:40 +0800

    tmp

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"bufio"
+	// "bytes"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+	// "golang.org/x/text/encoding/simplifiedchinese"
+	// "golang.org/x/text/transform"
+	"sync"
+	"strings"
+	"time"
 )
 
-	var ws *wsEngine
+var (
+	rwLock sync.RWMutex
+	wssObject = websocket.Upgrader{}
+	bufReaderList = make(map[string]*bufio.Reader)
+	wsConList = make(map[string]*websocket.Conn)
+	sh shCmd
+)
 
 type tst struct {
 	//
@@ -13,15 +27,59 @@ type tst struct {
 func main() {
 	r := gin.Default()
 			r.LoadHTMLGlob("./*.htm")
-					ws = NewEngineWs(Yml[shCmd]("c.yml").Sh)
 			r.GET("/", func(c *gin.Context) {
 					if c.Query("id") != "" {
-						ws.CliRegister(c.Query("id"), c.Writer, c.Request).RevBroadcastMsg()
+						con := wsConList[c.Query("id")]
+						if con == nil {
+						/*var err error;*/ con, _ = wssObject.Upgrade(c.Writer, c.Request, nil); wsConList[c.Query("id")] = con
+						}
+						defer con.Close()
+
+						rls := Yml[map[string]map[string][]string]("r.yml")
+
+						ver := time.Now().Format("v010206r")
+
+						if r,e := rls[ver]; !e {
+							for k,v := range sh.Sh {
+								rwLock.Lock(); bufReaderList[k] = Run(strings.Join(v, ";")); rwLock.Unlock()
+							}
+							rls[ver] = make(map[string][]string)
+							/////rwLock.Lock(); Write("r.yml", rls); rwLock.Unlock()
+							for{
+								if len(bufReaderList) == 0 {
+									break
+								}
+							for k, v := range bufReaderList {
+								//go func() {
+									//for {
+										log, _, err := v.ReadLine()
+										// reader := transform.NewReader(bytes.NewReader(log), simplifiedchinese.GBK.NewDecoder())
+										if err != nil /*|| io.EOF == err*/ {
+											delete(bufReaderList, k); delete(wsConList, c.Query("id")); break
+										}
+										con.WriteMessage(1, []byte("{\""+k+"\":\""+c.Query("id")+"--->"+string(log)+"\"}"))
+										kLog := rls[ver][k]
+										kLog = append(kLog, string(log))
+										rls[ver][k] = kLog
+										/////rwLock.Lock(); Write("r.yml", rls); rwLock.Unlock()
+										time.Sleep(
+											time.Millisecond * 100,
+										)
+									//}
+								//} ()
+							}
+							}
+						} else {
+							for k,v := range r {
+								con.WriteMessage(1, []byte("{\""+k+"\":\""+strings.Join(v, "\\n")+"\"}"))
+							}
+						}
+						
+						//
 					} else {
 						c.HTML(200, "default.htm", nil)
 					}
 			})
-					///ws.DestroyWs()
 	r.Run(":8080")
 }
 
@@ -32,5 +90,5 @@ type shCmd struct {
 }
 
 func init() {
-	//
+	sh = Yml[shCmd]("c.yml")
 }
