16dc7b3462edeb120e1b9d7d7d8deb5ddaebf9ad
root <lj@vipex.cc>
2022-07-11 22:45:46 +0800

    修复因 buf.UnreadByte() != nil 导致 进程卡死

--- shcmd.go
+++ shcmd.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "bufio"
-  "os"
-  "os/exec"
+  "fmt"
+  "bufio";"os";"os/exec"
+  "time"
 )
 
 func newSh(s string) *shCmd {
@@ -11,8 +11,12 @@ func newSh(s string) *shCmd {
       sh = &shCmd{ cmd: exec.Command("sh", "+xe", "-c", s, "2>&1") }
       sh.cmd.Stderr = os.Stderr; stdout, _ := sh.cmd.StdoutPipe(); //sh.cmd.Stdin = os.Stdin
       sh.cmd.Start()
+      time.Sleep(time.Millisecond * 100)
       b := bufio.NewReader(stdout)
-      go func() { for { log, err := b.ReadBytes('\n'); if err != nil /*|| io.EOF == err*/ { /**/; sh.isComplete = true; break }; sh.rst = append(sh.rst, string(log[:len(log)-1])) } } ()
+      go func() {
+      for { log, err := b.ReadBytes('\n'); if err != nil /*|| io.EOF == err*/ { /**/; sh.isComplete = true; break }; sh.rst = append(sh.rst, string(log[:len(log)-1])) }
+      } ()
+      fmt.Println(b)
   return sh
 }
 
