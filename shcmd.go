32378031ceeb37a46b2c8afb52e4417f6d0c711e
root <lj@vipex.cc>
2022-07-11 21:31:56 +0800

    .

--- shcmd.go
+++ shcmd.go
@@ -10,30 +10,20 @@ import (
 
 func newSh(s string) *shCmd {
 	var sh *shCmd
-	sh = &shCmd{cmd: exec.Command("sh", "+xe", "-c", s, "2>&1")}
-	sh.cmd.Stderr = os.Stderr
-	stdout, _ := sh.cmd.StdoutPipe() //sh.cmd.Stdin = os.Stdin
+	sh = &shCmd{ cmd: exec.Command("sh", "+xe", "-c", s, "2>&1") }
+	sh.cmd.Stderr = os.Stderr; stdout, _ := sh.cmd.StdoutPipe(); //sh.cmd.Stdin = os.Stdin
 	sh.cmd.Start()
-	time.Sleep(time.Millisecond * 1500)
+	time.Sleep(time.Millisecond * 100)
 	buf := bufio.NewReader(stdout)
 	fmt.Println(buf)
-	go func() {
-		for {
-			log, err := buf.ReadBytes('\n')
-			if err != nil /*|| io.EOF == err*/ { /**/
-				sh.isComplete = true
-				break
-			}
-			sh.rst = append(sh.rst, string(log[:len(log)-1]))
-		}
-	}()
+	go func() { for { log, err := buf.ReadBytes('\n'); if err != nil /*|| io.EOF == err*/ { /**/; sh.isComplete = true; break }; sh.rst = append(sh.rst, string(log[:len(log)-1])) } } ()
 	return sh
 }
 
 type shCmd struct {
 	//
-	cmd        *exec.Cmd
+	cmd *exec.Cmd
 	isComplete bool
-	rst        []string
+	rst []string
 	//
 }
