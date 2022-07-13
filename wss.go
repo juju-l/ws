9c8cfbf467c80ae6b5d29c51da981b901700b918
root <lj@vipex.cc>
2022-07-13 15:17:35 +0800

    全部通过

--- wss.go
+++ wss.go
@@ -26,24 +26,38 @@ func newWs() *wsEngine {
 func (ws *wsEngine) sendMsg(id string) {
 	s := 0
 
+	// for {
+	// 	if len(ws.rList) != 0 {
+	// 		break
+	// 	}
+	// }
+
+	i := 0
 	for {
-		if len(ws.rList) != 0 {
+		if s == len(ws.rList) {
+			//
+			delete(ws.wsConList, id)
 			break
 		}
-	}
 
-	for k, v := range ws.rList {
-		//k := m; v := *n
-		//go func() {
-			i := 0
-			for {
+
+		//
+		for k, v := range ws.rList {
 				if ws.shList[k].isComplete && len(*v) == i {
 					s++
-					break
-				}
-				if len(*v)-1-i < 0 {
 					continue
+					//
 				}
+
+				if len(*v)-1-i < 0 {
+					for { // 防止因时间差原因跳过本次数据，在此等待
+						time.Sleep(time.Millisecond*100)
+						if len(*v)-1-i >= 0{
+							break
+						}
+					}
+				}
+
 				time.Sleep(time.Millisecond*100)
 				err := ws.wsConList[id].WriteMessage(1, []byte("{\""+k+"\":\"--->"+(*v)[i]+"\"}"))
 				if err != nil {
@@ -51,18 +65,20 @@ func (ws *wsEngine) sendMsg(id string) {
 					delete(ws.wsConList, id)
 					return
 				}
-				i++
-			}
-		//}()
-	}
-
-	for {
-		if s == len(ws.rList) {
-			//
-			delete(ws.wsConList, id)
-			break
 		}
+		i++ //需要确保上面本次 i 的所有数组都取过一次，如果没有取到，除非本身不存在外，需要在上边进行手工等待，防止因时间差原因而错行
+
+
 	}
+	//
+
+	// for {
+	// 	if s == len(ws.rList) {
+	// 		//
+	// 		delete(ws.wsConList, id)
+	// 		break
+	// 	}
+	// }
 }
 
 func (ws *wsEngine) run(cfg *appConfig) *wsEngine {
